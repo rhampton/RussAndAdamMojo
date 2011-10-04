0a36ce11a23694eb8b43e44b6e8297055ed5bfbc
Adam Dickison <adam.dickison@gmail.com>
2011-10-04 13:31:29 -0400

    Initial Check In

---
+++ KemiApp/KemiApp/KemiAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KemiAppAppDelegate.h
+//  KemiApp
+//
+//  Created by KEMI on 10/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KemiAppAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
